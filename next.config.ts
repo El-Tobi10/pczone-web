f96fe5160c7501c359f94061ba4974befd89b152
Gabriel <domingogaby8@gmail.com>
2025-05-18 21:04:05 -0300

    Ajuste next.config para aceptar varios formatos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'pcsupptnitvozhbhfxiu.supabase.co', // tu proyecto en Supabase
+      'i.imgur.com', // si usás Imgur
+      // agregá más si hace falta
+    ],
+  },
 };
 
 export default nextConfig;
